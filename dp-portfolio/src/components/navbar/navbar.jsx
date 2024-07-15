a1107ae4807069637cfdae82371098cc298e15a8
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-07-16 01:27:57 +0530

    image setups

--- dp-portfolio/src/components/navbar/navbar.jsx
+++ dp-portfolio/src/components/navbar/navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "../navbar.module.css";
+import { Grid } from "@mui/material";
+import myBlackLogo from "../../assets/images/navbar-icons/black.png";
+import myWhiteLogo from "../../assets/images/navbar-icons/black.png";
 
 export default function Navbar() {
   return (
     // navbar container
     <div>
       <Grid container>
-
+        <Grid item xs={1} />
+        <Grid container item xs={10} className={styles.navbarContainer}>
+          <Grid item xs={3} style={{ display: "flex" }}></Grid>
+          <Grid item xs={8}>
+            <span></span>
+          </Grid>
+          <Grid item xs={1}></Grid>
+        </Grid>
       </Grid>
     </div>
     // navbar container
--- dp-portfolio/src/pages/Main.jsx
+++ dp-portfolio/src/pages/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../components/navbar/navbar";
+import styles from "../main.module.css";
 
 export default function Main() {
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <Navbar />
     </div>
   );
