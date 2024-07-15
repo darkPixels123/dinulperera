9036b389b49923393550601c919748450a7e4521
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-07-16 00:53:23 +0530

    initial setups

--- dp-portfolio/src/App.js
+++ dp-portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Dinul Perera</h1>
+      <Main />
     </div>
   );
 }
---
+++ dp-portfolio/src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../navbar.module.css";
+
+export default function Navbar() {
+  return (
+    // navbar container
+    <div>
+      <Grid container>
+
+      </Grid>
+    </div>
+    // navbar container
+  );
+}
