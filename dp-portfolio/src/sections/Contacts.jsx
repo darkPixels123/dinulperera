27ae5101175f43d3d46041d796faf11ff62eb504
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-09-14 17:05:27 +0530

    testing changes

--- dp-portfolio/src/sections/Contacts.jsx
+++ dp-portfolio/src/sections/Contacts.jsx
@@ -74,6 +74,7 @@ export default function Contacts({ isDarkMode }) {
                         }}
                       >
                         <img
+                          alt="social_img"
                           src={
                             isDarkMode ? item.whiteImageUrl : item.blackImageUrl
                           }
@@ -101,6 +102,7 @@ export default function Contacts({ isDarkMode }) {
               }}
             >
               <iframe
+                title="location"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.2420699409435!2d79.98871256958446!3d6.894397630976664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae251490a54f23b%3A0x9e46305451b3306a!2s69%20Sugathi%20Mawatha!5e0!3m2!1sen!2slk!4v1726235433555!5m2!1sen!2slk"
                 style={{
                   border: 0,
