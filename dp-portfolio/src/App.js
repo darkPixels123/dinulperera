e4c30d1888dccde3be43a621637edaf90f8d81e4
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-09-14 22:59:58 +0530

    changes to loader pt1

--- dp-portfolio/src/App.js
+++ dp-portfolio/src/App.js
@@ -4,26 +4,23 @@ import Main from './sections/Main';
 import Loading from './sections/Loading';
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    return localStorage.getItem("selectedTheme") === "dark";
-  });
+  const [isDarkMode, setIsDarkMode] = useState(
+    localStorage.getItem("selectedTheme") === "dark"
+  );
 
   const [isLoading, setIsLoading] = useState(true);
 
+  // loading animation
   useEffect(() => {
-    const handleContentLoaded = () => setIsLoading(false);
-    document.addEventListener("DOMContentLoaded", handleContentLoaded);
-
-    return () => {
-      document.removeEventListener("DOMContentLoaded", handleContentLoaded);
-    };
+    window.addEventListener("DOMContentLoaded", function () {
+      setIsLoading(false)
+    })
   }, []);
-
-  const containerClass = isDarkMode ? 'dark-background' : 'light-background';
+  // loading animation
 
   return (
-    <div className={`App ${containerClass}`}>
-      {isLoading ? <Loading isDarkMode={isDarkMode} /> : <Main />}
+    <div className="App">
+      {isLoading ? (<div style={{ backgroundColor: isDarkMode ? ("rgb(18, 18, 18)") : ("rgb(255, 255, 255)") }}><Loading isDarkMode={isDarkMode} /></div>) : (<Main />)}
     </div>
   );
 }
